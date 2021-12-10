649bd01e4ca73827136c95cd5a302e827c36bc83
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-12-10 17:01:58 +0000

    testing

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+
+// to make a new node project, require the express module
 const express = require("express");
 
+// initialise a new express app
 const app = express();
 const port = 3000;
 
+// make app listen to port 3000
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
