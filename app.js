ac9c1a830e16c9072dee3f82e6e3471c5345b090
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-12-10 17:17:51 +0000

    testing external api call

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 // to make a new node project, require the express module
 const express = require("express");
+const https = require("https");
 
 // initialise a new express app
 const app = express();
@@ -11,8 +12,15 @@ app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
 
-// GET REQUEST
+// GET REQUEST - run server via nodemon
 
 app.get("/", (req, res) => {
+  // we can call the https module, a native node module, to make a get request to an API
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=44579a6b9e1e41782e49cbbc73acfa42&units=metric"
+  // the https.get method takes a url for the API we are making a get request to and a callback function
+  https.get(url, (response) => {
+    // lets test if the get request works
+    console.log(response)
+  })
   res.send("This app is working.")
 })
